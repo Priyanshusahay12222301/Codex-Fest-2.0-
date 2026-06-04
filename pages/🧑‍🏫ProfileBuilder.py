57e1af03d20c73765a1485769c14b67846863afa
Priyanshusahay12222301 <119564402+Priyanshusahay12222301@users.noreply.github.com>
2026-06-04 12:30:38 +0530

    Fix: wrap Firebase init in try/except to prevent crash when secrets are missing

--- pages/🧑‍🏫ProfileBuilder.py
+++ pages/🧑‍🏫ProfileBuilder.py
@@ -52,25 +52,28 @@ from firebase_admin import credentials
 from firebase_admin import db
 import time
 
-if not firebase_admin._apps:
-    
-    service_account_info = {
-        "type": "service_account",
-        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": (os.environ.get("FIREBASE_PRIVATE_KEY") or "").replace("\\n", "\n"),  # Important: handle newlines
-        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
-        "universe_domain": "googleapis.com"
-    }
-    cred = credentials.Certificate(service_account_info)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': "https://profile-data-dde0a-default-rtdb.firebaseio.com/"
-    })
+try:
+    if not firebase_admin._apps:
+        service_account_info = {
+            "type": "service_account",
+            "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": (os.environ.get("FIREBASE_PRIVATE_KEY") or "").replace("\\n", "\n"),
+            "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL"),
+            "universe_domain": "googleapis.com"
+        }
+        cred = credentials.Certificate(service_account_info)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': "https://profile-data-dde0a-default-rtdb.firebaseio.com/"
+        })
+except Exception as _firebase_err:
+    import streamlit as _st
+    _st.warning("Firebase not configured: set FIREBASE_* secrets in Streamlit Cloud to enable profile features.")
   
 global s
 k=0
